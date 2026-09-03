34f52bdb35a34e3904a6efa83bc3f9b6ec49e2bf
Saniya Inamdar <149571751+sanoInamdar@users.noreply.github.com>
2026-09-03 23:18:30 +0530

    Time: 2 ms (100.00%) | Memory: 121.6 MB (74.64%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+
+
+        int min = nums1[0];
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+        }
+
+       
+        if (min % 2 == 1) {
+            return true;
+        }
+
+        
+        for (int num : nums1) {
+            if (num % 2 == 1) {
+                return false;
+            }
+        }
+
+        // All elements are even already.
+        return true;
+    }
+}
+
