9ff214bdb6e9f0e76ea8ee1e2d92d339294d147c
Saniya Inamdar <149571751+sanoInamdar@users.noreply.github.com>
2026-09-14 13:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.92%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        // Check horizontal overlap
+        boolean horizontal = rec1[0] < rec2[2] && rec2[0] < rec1[2];
+
+        // Check vertical overlap
+        boolean vertical = rec1[1] < rec2[3] && rec2[1] < rec1[3];
+
+        return horizontal && vertical;
+    
+        
+    }
+}
